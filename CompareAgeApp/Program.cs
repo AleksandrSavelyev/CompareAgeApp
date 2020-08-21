f6ddad3046320c697c03da881412addb354c1a9b
AleksandrSavelyev <punk558721973@gmail.com>
2020-08-21 10:53:00 +0300

    master branch

---
+++ CompareAgeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompareAgeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
